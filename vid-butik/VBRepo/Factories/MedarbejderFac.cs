cb3f67decc9eee9a2a9abee8af0c01cb45fdacb0
Lucas Romanini <Lucas@romanini.dk>
2017-04-27 09:16:46 +0200

    push 27?

--- vid-butik/VBRepo/Factories/MedarbejderFac.cs
+++ vid-butik/VBRepo/Factories/MedarbejderFac.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VBRepo.Factories
+namespace VBRepo
 {
     public class MedarbejderFac : AutoFac<Medarbejder>
     {
--- vid-butik/vid-butik/Controllers/IndexController.cs
+++ vid-butik/vid-butik/Controllers/IndexController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VBRepo;
 
 namespace vid_butik.Controllers
 {
     public class IndexController : Controller
     {
+        IndexFac IF = new IndexFac();
+        KontaktFac KF = new KontaktFac();
+        MedarbejderFac MF = new MedarbejderFac();
+        Om_OsFac OOF = new Om_OsFac();
+        Vare_infoFac VIF = new Vare_infoFac();
+
         // GET: Index
         public ActionResult Index()
         {
@@ -15,7 +22,7 @@ namespace vid_butik.Controllers
         }
         public ActionResult Kontakt()
         {
-            return View();
+            return View(KF.Get(1));
         }
         public ActionResult omos()
         {
