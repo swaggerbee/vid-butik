7ff2546cfe6dcb0b5cbe1812221fe488a85b5a2f
Lucas-romanini <lucas@romanini.dk>
2017-05-31 10:19:00 +0200

    mail form testing

--- vid-butik/vid-butik/Controllers/IndexController.cs
+++ vid-butik/vid-butik/Controllers/IndexController.cs
@@ -6,6 +6,10 @@ using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Web.Security;
 using VBRepo;
+using MVCEmail.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace vid_butik.Controllers
 {
@@ -68,6 +72,43 @@ namespace vid_butik.Controllers
         {
             FormsAuthentication.SignOut();
             return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("luca0327@videndjurs.net"));  // replace with valid value 
+                message.From = new MailAddress("sender@outlook.com");  // replace with valid value
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "luca0327@videndjurs.net",  // replace with valid value
+                        Password = "killemall1983"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp-mail.outlook.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Sent()
+        {
+            return View();
         }
     }
 }
