dcd6dacb6c52243c0d4cdd4ef2dc55c4393b5329
Andreas Brunn Dam Larsen <fiske.unger@gmail.com>
2017-06-01 09:22:34 +0200

    validate på red,slet,gem

---
+++ vid-butik/vid-butik/Content/validate.js
@@ -0,0 +1,16 @@
+﻿function ValidateGem() {
+    var txt;
+    if (confirm("er du sikker på du vil gemme!") == true) {
+        txt = "You pressed OK!";
+    } else {
+        txt = "You pressed Cancel!";
+    }
+}
+function ValidateSlet() {
+    var txt;
+    if (confirm("er du sikker på du vil slette!") == true) {
+        txt = "You pressed OK!";
+    } else {
+        txt = "You pressed Cancel!";
+    }
+}
