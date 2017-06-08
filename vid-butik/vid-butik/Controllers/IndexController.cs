7f3898032b899e1480a89436b2e1603a497610b6
Lucas Kofoed ROmanini <lucas@romanini.dk>
2017-06-09 01:25:31 +0200

    kontakt form -skal lige snakke med David om det-

--- vid-butik/vid-butik/Controllers/IndexController.cs
+++ vid-butik/vid-butik/Controllers/IndexController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using System.Web.Security;
 using VBRepo;
 using MVCEmail.Models;
+using System.Text;
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
@@ -72,38 +73,6 @@ namespace vid_butik.Controllers
         {
             FormsAuthentication.SignOut();
             return RedirectToAction("Index");
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Contact(EmailFormModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
-                var message = new MailMessage();
-                message.To.Add(new MailAddress("luca0327@videndjurs.net"));  // replace with valid value 
-                message.From = new MailAddress("sender@outlook.com");  // replace with valid value
-                message.Subject = "Your email subject";
-                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
-                message.IsBodyHtml = true;
-
-                using (var smtp = new SmtpClient())
-                {
-                    var credential = new NetworkCredential
-                    {
-                        UserName = "luca0327@videndjurs.net",  // replace with valid value
-                        Password = "killemall1983"  // replace with valid value
-                    };
-                    smtp.Credentials = credential;
-                    smtp.Host = "smtp-mail.outlook.com";
-                    smtp.Port = 587;
-                    smtp.EnableSsl = true;
-                    await smtp.SendMailAsync(message);
-                    return RedirectToAction("Sent");
-                }
-            }
-            return View(model);
         }
 
         public ActionResult Sent()
---
+++ vid-butik/vid-butik/Controllers/KontaktFormSurefaceController.cs
@@ -0,0 +1,79 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
+//using VBRepo;
+//using System.Text;
+//using System.Net;
+//using System.Net.Mail;
+
+
+
+//namespace vid_butik.Controllers
+//{
+//    public class KontaktFormSurefaceController : Controller
+//    {
+//        // GET: KontaktFormSureface
+//        public ActionResult Index()
+//        {
+//            return PartialView ("KontaktForm", new contactModel());
+//        }
+
+//        public ActionResult HandleFormSUbmit()
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                string brugernavn = CurrentPage.GetPropertyValue<string>("brugernavn");
+//                string kodeord = CurrentPage.GetPropertyValue<string>("kodeord");
+//                string mailServer = CurrentPage.GetPropertyValue<string>("mailServer");
+//                int portNummer = CurrentPage.GetPropertyValue<string>("portNummer");
+//                string emailFra = CurrentPage.GetPropertyValue<string>("emailFra");
+//                string emailTil = CurrentPage.GetPropertyValue<string>("emailTil");
+
+//                SmtpClient smpt = new SmtpClient();
+//                smpt.UseDefaultCredentials = false;
+//                smpt.Credentials = new NetworkCredential(brugernavn, kodeord);
+//                smpt.Host = mailServer;
+//                smpt.Port = portNummer;
+//                smpt.DeliveryMethod = SmtpDeliveryMethod.Network;
+//                smpt.EnableSsl = false;
+
+//                string mailFrom = emailFra;
+//                string mailTo = emailTil;
+
+//                string mailSubject = "Emaiil fra Hjemmesiden";
+
+//                StringBuilder mailContent = new StringBuilder();
+//                mailContent.Append("<h3>Kontakt oplysninger:</h3>");
+//                mailContent.AppendFormat("Navn: <b>{0}</b><br />", Model.Navn);
+//                mailContent.AppendFormat("Kontakt: <b>{0}</b>", Model.Info);
+//                mailContent.AppendFormat("<hr />{0}", ModelBinderAttribute.Besked.Replace(Environment.NewLine, "<br />"));
+
+//                MailMessage mail = new MailMessage(mailFrom, mailTo, mailSubject, mailContent.ToString());
+//                mail.IsBodyHtml = true;
+//                mail.HeadersEncoding = Encoding.UTF8;
+//                mail.BodyEncoding = Encoding.UTF8;
+//                mail.SubjectEncoding = Encoding.UTF8;
+//                try
+//                {
+//                    smpt.Send(mail);
+//                }
+//                catch (SmtpException sex)
+//                {
+//                    //TemData["MailFormSuccess"]=sex.Message;
+//                }
+//                catch (Exception ex)
+//                {
+//                    //TemData["MailFormSuccess"]=ex.Message;
+//                }
+//                return RedirectToAction("");
+
+//            }
+//            else
+//            {
+//                return RedirectToAction("");
+//            }
+//        }
+//    }
+//}
